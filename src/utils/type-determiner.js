a7ed8887e2b2e574bbf563745e5e2875dd894ee5
Mhmdrz <mr.ala72@gmail.com>
2019-01-10 01:44:04 +0330

    bugfixes and functionality improved

--- src/utils/type-determiner.js
+++ src/utils/type-determiner.js
@@ -1,56 +1,59 @@
 function triangleTypeDeterminer(side1, side2, side3) {
-    //all the inputs should be number 
-    if( isNaN(side1) || isNaN(side2) || isNaN(side3) ){
-      return "Inputs are invalid"
-    }
-    // set will remove duplicate elemnts thus we can determine
-    // type of triangle by the number of elements of the set
-    const set = new Set([side1,side2,side3]);
-    let typeOfTriangle;
-    switch (set.size) {
-      case 1:
-        typeOfTriangle = "equilateral"
-        break;
-      case 2:
-        typeOfTriangle = canBeATriangle(set) ? "isosceles" : "These values can't form a triangle"
-        break;
-      case 3:
-        typeOfTriangle = canBeATriangle(set) ? "scalene" : "These values can't form a triangle"
-        break;  
-      default:
-        // this will never happen
-        typeOfTriangle = "JS Engine is Broken :("
-        break;
-    }
-    return typeOfTriangle
+  //all the inputs should be number 
+  if( typeof side1 !== "number" 
+      || typeof side2 !== "number" 
+        || typeof side3 !== "number"){
+    return "Inputs are invalid"
   }
-
-
-function canBeATriangle( triangleSidesSet ){
-  let canBe;
-  const arrayOfSides = Array.from(triangleSidesSet)
-  // there is no need to think about the best way of sorting because
-  // there wouldn't be more than 3 elements 
-  arrayOfSides.sort()
-  // sorting will provide the knowledge of sides 
-
-  //mathematically if the biggest side be bigger than sum of
-  // two other sides, those number won't make a triangle
-  switch (arrayOfSides.length) {
+  if( side1 <= 0 || side2 <= 0 || side3 <= 0){
+    return "non-zero positive values are expected"
+  }
+  // set will remove duplicate elemnts thus we can determine
+  // type of triangle by the number of elements of the set
+  var arrayOfSides = [side1, side2, side3]
+  var setOfSides = new Set(arrayOfSides)
+  var typeOfTriangle = "These values can't form a triangle";
+  switch (setOfSides.size) {
+    case 1:
+      typeOfTriangle = "An equilateral triangle"
+    break;
     case 2:
-      const smallerSides = parseFloat(arrayOfSides)[0]
-      const biggerSide = parseFloat(arrayOfSides)[1]
-      canBe = biggerSide < (2 * smallerSides)
+      if(canBeATriangle(arrayOfSides))
+        typeOfTriangle = "An isosceles triangle"
       break;
     case 3:
-      const smallestSide = parseFloat(arrayOfSides)[0]
-      const middleSide = parseFloat(arrayOfSides)[1]
-      const biggestSide = parseFloat(arrayOfSides)[2]
-      canBe = biggestSide < (smallestSide + middleSide)
-      break;
-  
+      if(canBeATriangle(arrayOfSides))
+        typeOfTriangle = "A scalene triangle" 
+      break;  
     default:
+      // this will never happen
+      typeOfTriangle = "JS Engine is Broken :("
       break;
+  }
+  return typeOfTriangle
+}
+
+function canBeATriangle(triangleSidesArray){
+  var canBe;
+  //copying the array to avoid any side effect
+  var triangleSidesArrayCopy = triangleSidesArray.slice()
+  // there is no need to think about the best way of sorting because
+  // there wouldn't be more than 3 elements 
+  triangleSidesArrayCopy.sort();
+  // mathematically if the biggest side of the triangle 
+  // be bigger than sum of two other sides, those number
+  // won't make a triangle
+
+  // sorting will provide the knowledge of sides 
+  var [small, medium, big] = triangleSidesArrayCopy;
+  if( small === medium || medium === big){
+    if(small === medium){
+      canBe = big < 2 * medium
+    }else{
+      canBe = small < 2 * medium
+    }
+  }else{
+    canBe = big < small + medium
   }
   return canBe
 }
