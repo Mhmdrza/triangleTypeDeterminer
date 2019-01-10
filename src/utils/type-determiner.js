60aa111e4db5724070057724229f6a63b4bdc761
mr.ala <mr.ala72@gmail.com>
2019-01-11 02:02:56 +0330

    response type changed

--- src/utils/type-determiner.js
+++ src/utils/type-determiner.js
@@ -1,36 +1,48 @@
 function triangleTypeDeterminer(side1, side2, side3) {
+  //default values
+  var response = {
+    isTriangle: false,
+    description: "These values can't form a triangle"
+  }
+
   //all the inputs should be number 
   if( typeof side1 !== "number" 
       || typeof side2 !== "number" 
         || typeof side3 !== "number"){
-    return "Inputs are invalid"
+    response.description = "Inputs are invalid"
+    return response
   }
+
   if( side1 <= 0 || side2 <= 0 || side3 <= 0){
-    return "non-zero positive values are expected"
+    response.description = "non-zero positive values are expected"
+    return response
   }
+
   var arrayOfSides = [side1, side2, side3]
   // set will remove duplicate elemnts thus we can determine
   // type of triangle by the number of elements of the set
   var setOfSides = new Set(arrayOfSides)
-  var typeOfTriangle = "These values can't form a triangle";
   switch (setOfSides.size) {
     case 1:
-      typeOfTriangle = "An equilateral triangle"
+      successResponse("An equilateral triangle")
     break;
     case 2:
       if(canBeATriangle(arrayOfSides))
-        typeOfTriangle = "An isosceles triangle"
+        successResponse("An isosceles triangle")
       break;
     case 3:
       if(canBeATriangle(arrayOfSides))
-        typeOfTriangle = "A scalene triangle" 
+        successResponse("A scalene triangle")
       break;  
     default:
-      // this will never happen
-      typeOfTriangle = "JS Engine is Broken :("
       break;
   }
-  return typeOfTriangle
+
+  function successResponse ( typeOftriangle ) {
+    response.description = typeOftriangle
+    response.isTriangle = true
+  }
+  return response
 }
 
 function canBeATriangle(triangleSidesArray){
@@ -38,7 +50,9 @@ function canBeATriangle(triangleSidesArray){
   var triangleSidesArrayCopy = triangleSidesArray.slice()
   // there is no need to think about the best way of sorting because
   // there wouldn't be more than 3 elements 
-  triangleSidesArrayCopy.sort();
+
+  // ascending sort function for numbers 
+  triangleSidesArrayCopy.sort((function(a, b){return a - b}));
   // mathematically if the biggest side of the triangle 
   // be bigger than sum of two other sides, those number
   // won't make a triangle
