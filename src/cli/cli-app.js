9c4439570edc2d75e4344836e7a41e41445c2797
Mhmdrz <mr.ala72@gmail.com>
2019-01-11 18:06:09 +0330

    modified to wrok with new response type, and alittle refactoring

--- src/cli/cli-app.js
+++ src/cli/cli-app.js
@@ -1,5 +1,9 @@
 var inquirer = require('inquirer')
 var triangleTypeDeterminer = require('../utils/type-determiner')
+var validate = function(value) {
+  var valid = !isNaN(parseFloat(value));
+  return valid || 'Please enter a number';
+}
 
 console.log('\nTriangle Type Determiner application\n');
 
@@ -8,34 +12,26 @@ let questions = [
     type: 'input',
     name: 'side1',
     message: 'Enter the value of first Side of triangle',
-    validate: function(value) {
-      var valid = !isNaN(parseFloat(value));
-      return valid || 'Please enter a number';
-    },
+    validate: validate,
     filter: Number
   },
   {
     type: 'input',
     name: 'side2',
     message: 'Enter the value of second Side of triangle',
-    validate: function(value) {
-      var valid = !isNaN(parseFloat(value));
-      return valid || 'Please enter a number';
-    },
+    validate: validate,
     filter: Number
   },
   {
     type: 'input',
     name: 'side3',
     message: 'Enter the value of third Side of triangle',
-    validate: function(value) {
-      var valid = !isNaN(parseFloat(value));
-      return valid || 'Please enter a number';
-    },
+    validate: validate,
     filter: Number
   }
 ];
 
 inquirer.prompt(questions).then(answers => {
-  console.log('\n',triangleTypeDeterminer(...Object.values(answers)),'\n');
+  var response = triangleTypeDeterminer(...Object.values(answers))
+  console.log('\n',response.description,'\n');
 });
