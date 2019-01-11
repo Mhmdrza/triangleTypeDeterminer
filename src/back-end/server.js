c3b85565095b1dd0dd8a9767f467462ec7d45911
Mhmdrz <mr.ala72@gmail.com>
2019-01-11 18:06:41 +0330

    post requests support added

--- src/back-end/server.js
+++ src/back-end/server.js
@@ -1,22 +1,38 @@
-var express = require('express');
-var app = express();
 var triangleTypeDeterminer = require('../utils/type-determiner')
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/typeDeterminer', function(req, res) {
-    try{
-        var par1 = parseFloat(req.query.side1)
-        var par2 = parseFloat(req.query.side2)
-        var par3 = parseFloat(req.query.side3)
-    }catch{
-        res.writeHead( 200 );
-        res.write( JSON.stringify( 'invalid data type' ) );
-        res.end();;
+    var isAccepted = true;
+    var par1 = parseFloat(req.query.side1)
+    var par2 = parseFloat(req.query.side2)
+    var par3 = parseFloat(req.query.side3)
+    if( isNaN(par1) || isNaN(par2) || isNaN(par3)){
+        isAccepted = false
     }
+    if(isAccepted){
+        var response = triangleTypeDeterminer(par1, par2, par3)   
+        res.setHeader('Content-Type', 'application/json'); 
+        res.send(JSON.stringify(response));
+    }else{
+        res.status(400);
+        res.send('Expected a number as parameter');
+    }
+});
+
+app.post('/typeDeterminer', function(req, res) {
+    var par1 = parseFloat(req.body.side1)
+    var par2 = parseFloat(req.body.side2)
+    var par3 = parseFloat(req.body.side3)
     var response = triangleTypeDeterminer(par1, par2, par3)   
     res.setHeader('Content-Type', 'application/json'); 
     res.send(JSON.stringify(response));
- });
- 
+});
+
 app.listen(5002);
 console.log('Listening on port 5002...');
 
